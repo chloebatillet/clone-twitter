3a584698f210cb2178a5dff8da3818791975afe1
chloebatillet <cbatillet@gmail.com>
2023-06-13 12:38:39 +0200

    :construction: Added number of likes and retweet to getAll + order DESC
    Same for getOne + working on catching all replies for one tweet

--- API/controllers/tweetController.js
+++ API/controllers/tweetController.js
@@ -1,10 +1,47 @@
 const Tweet = require('../models/Tweet');
+//const Like = require('../models/Like');
 
+const sequelize = require('../database')
 
 module.exports = {
     async getAll(_, res) {
         const result = await Tweet.findAll({
-            include: ['retweet', 'like']
+            include: ['retweet', 'like'],
+            order: [
+                ['created_at', 'DESC']
+            ],
+            attributes: {
+                include: [
+                    [sequelize.literal('(SELECT COUNT(*) FROM "like" WHERE "like"."tweet_id" = "Tweet"."id")'),
+                    'number_like'
+                    ],
+                    [sequelize.literal('(SELECT COUNT(*) FROM "retweet" WHERE "retweet"."tweet_id" = "Tweet"."id")'),
+                    'number_retweet'
+                    ]
+                ]
+            }
+        })
+        res.json(result)
+    },
+
+    async getOne(req, res) {
+        const id = Number(req.params.id);
+
+        const result = await Tweet.findByPk(id, {
+            include: ['retweet', 'like'],
+            order: [
+                ['created_at', 'DESC']
+            ],
+            attributes: {
+                include: [
+                    [sequelize.literal('(SELECT COUNT(*) FROM "like" WHERE "like"."tweet_id" = "Tweet"."id")'),
+                    'number_like'
+                    ],
+                    [sequelize.literal('(SELECT COUNT(*) FROM "retweet" WHERE "retweet"."tweet_id" = "Tweet"."id")'),
+                    'number_retweet'
+                    ]
+                ]
+            }
         })
         res.json(result)
     }
